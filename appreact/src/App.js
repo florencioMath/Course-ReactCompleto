44bb5af6a161f09e866bbc9e6647276ae14657b0
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-06 11:20:56 -0300

    feat: Exercício useState

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Produto from "./Produto";
 
 const App = () => {
-  return <div>App React</div>;
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
+  return (
+    <>
+      <div>App React</div>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        smartphone
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        tablet
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        notebook
+      </button>
+      {carregando && <p>Carregando..</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </>
+  );
 };
 
 export default App;
