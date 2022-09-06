3d5212b8de2e7c0c6a9048ccb49637066201de83
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-05 21:34:48 -0300

    fix: Reformulação do código

--- ReactParaIniciantes/NpxCreateReactApp/meuapp/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <div>App do Matheus.</div>;
-};
-
-export default App;
--- ReactParaIniciantes/WebpackEBabel/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./App.css";
-
-const App = () => {
-  return <a href="https://github.com/florencioMath">Matheus</a>;
-};
-
-export default App;
