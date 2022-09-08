c96210e7ef94d765089cbefcc0d82c21093d6ac8
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-08 14:45:47 -0300

    feat: useContext exercício

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,31 +1,14 @@
 import React from "react";
+import { GlobalStorage } from "./GlobalContext";
 import Produto from "./Produto";
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
-
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem("produto");
-    if (produtoLocal !== "null") setProduto(produtoLocal);
-  }, []);
-
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem("produto", produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
-
   return (
     <>
       <div>App React</div>
-      <h1>Preferência: {produto}</h1>
-      <button style={{ marginRight: ".5rem" }} onClick={handleClick}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-      <Produto produto={produto} />
+      <GlobalStorage>
+        <Produto />
+      </GlobalStorage>
     </>
   );
 };
