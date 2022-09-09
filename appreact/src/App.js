59206d564b1d7aa94edd5ed43d370b2c43b78849
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-09 09:36:01 -0300

    feat: Limpeza do ambiente de desenvolvimento

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { GlobalStorage } from "./GlobalContext";
-import Produto from "./Produto";
 
 const App = () => {
   return (
     <>
       <div>App React</div>
-      <GlobalStorage>
-        <Produto />
-      </GlobalStorage>
     </>
   );
 };
