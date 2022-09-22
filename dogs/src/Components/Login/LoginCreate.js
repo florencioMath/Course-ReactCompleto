4fffc9148ccdad613d75959f738e65141242701c
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-22 09:57:38 -0300

    feat: useFetch

--- dogs/src/Components/Login/LoginCreate.js
+++ dogs/src/Components/Login/LoginCreate.js
@@ -4,6 +4,8 @@ import Input from "../../Components/Forms/Input";
 import Button from "../../Components/Forms/Button";
 import { USER_POST } from "../../api";
 import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,7 +22,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -30,8 +33,9 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="text" name="email" {...email} />
         <Input label="Senha" type="text" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? <Button>Cadastrando...</Button> : <Button>Cadastrar</Button>}
       </form>
+      <Error error={error} />
     </section>
   );
 };
