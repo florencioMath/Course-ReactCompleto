4568c5438fb1b25d65809afe7ff45674ff294376
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-09-29 14:08:35 -0300

    feat: Feed Modal 2

--- dogs/src/Components/Photo/PhotoContent.js
+++ dogs/src/Components/Photo/PhotoContent.js
@@ -10,9 +10,9 @@ const PhotoContent = ({ data }) => {
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
-      <div className={styles.detail}>
+      <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
