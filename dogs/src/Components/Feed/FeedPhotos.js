d488a80ace9c93fc3f498095c9dbc0bf9ce2a475
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-01 18:23:43 -0300

    feat: Scroll Infinito

--- dogs/src/Components/Feed/FeedPhotos.js
+++ dogs/src/Components/Feed/FeedPhotos.js
@@ -6,16 +6,19 @@ import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
+    const total = 3;
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total)
+        return setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
