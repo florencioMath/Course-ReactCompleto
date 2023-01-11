adb16c695ef7281a3d62a608556d583a18fdcfed
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-10 21:42:34 -0300

    feat: Mudança no path do login e novo build

--- dogs/src/Components/Login/Login.js
+++ dogs/src/Components/Login/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     <section className={styles.login}>
       <div className={styles.forms}>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path={process.env.PUBLIC_URL + "/"} element={<LoginForm />} />
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
